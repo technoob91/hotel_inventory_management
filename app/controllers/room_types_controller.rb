24d882a79e321cfdd4594d5e991d796e17ded43f
Aashish Ramalingam <aashish.11ramalingam@gmail.com>
2019-06-21 22:10:44 +0530

    refactor

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -10,17 +10,12 @@ class RoomTypesController < ApplicationController
     availability = AvailabilityService.get_availability(params)
     
     response = {
-      price: prices(params, availability[:room_id]) || 0,
+      price: PriceService.get_price(params, availability[:room_id]) || 0,
       available: availability[:available],
       room: availability[:room_id]
     }
     render(json: response)
   end
-  
-  def prices(params)
-    PriceService.get_price(params)
-    # render(json: room_price)
-  end  
 
   def show
     @room_types = RoomType.where(property_id: params[:property_id])
