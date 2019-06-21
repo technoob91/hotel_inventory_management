8b99ca49d35c7a5b7a06861f7572acc20e223e22
Aashish Ramalingam <aashish.11ramalingam@gmail.com>
2019-06-21 21:57:42 +0530

    refactor

--- db/migrate/20190619203100_remove_room_from_daily_rates.rb
+++ db/migrate/20190619203100_remove_room_from_daily_rates.rb
@@ -1,6 +1,5 @@
 class RemoveRoomFromDailyRates < ActiveRecord::Migration[5.2]
   def change
     add_reference :daily_rates, :room_type, foreign_key: true
-    remove_reference :daily_rates, :room, foreign_key: true
   end
 end
--- db/migrate/20190621162127_add_room_to_daily_price.rb
+++
@@ -1,5 +0,0 @@
-class AddRoomToDailyPrice < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :daily_prices, :room, foreign_key: true
-  end
-end
